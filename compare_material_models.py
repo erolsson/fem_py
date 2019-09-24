a7593682ec36b40a5b295cd58abe9872b0d414ff
erolsson@kth.se <erolsson@kth.se>
2019-09-24 16:00:38 +0200

    "implementing umat in c++"

--- compare_material_models.py
+++ compare_material_models.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 
+import os
+
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -12,7 +14,7 @@ from materials.SS2506 import test_material
 Simulation = namedtuple('Simulation', ['model', 'label', 'color', 'umat_file'])
 simulations = [  # Simulation(model=one_element_simulation, label='New', color='b', umat_file=None),
                Simulation(model=one_element_abaqus, label='Abaqus', color='r',
-                          umat_file='transformation_subroutines/transformation_umat.o')]
+                          umat_file=os.path.expanduser('~/fem_py/transformation_subroutines/transformation_umat.o'))]
 
 increments = 1000
 
