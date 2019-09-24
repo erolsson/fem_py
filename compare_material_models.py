c4ddeb19ae90a10ef4e97272d18a44bd427c29f1
erolsson@kth.se <erolsson@kth.se>
2019-09-24 15:53:02 +0200

    "implementing umat in c++"

--- compare_material_models.py
+++ compare_material_models.py
@@ -4,13 +4,13 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from abaqus_material_test.material_test import one_element_abaqus
-from one_element_test import one_element_simulation
+from .abaqus_material_test.material_test import one_element_abaqus
+from .one_element_test import one_element_simulation
 
-from materials.SS2506 import test_material
+from .materials.SS2506 import test_material
 
 Simulation = namedtuple('Simulation', ['model', 'label', 'color', 'umat_file'])
-simulations = [Simulation(model=one_element_simulation, label='New', color='b', umat_file=None),
+simulations = [  # Simulation(model=one_element_simulation, label='New', color='b', umat_file=None),
                Simulation(model=one_element_abaqus, label='Abaqus', color='r',
                           umat_file='transformation_subroutines/transformation_umat.o')]
 
