5996f8bf2dae765d1b0b433cd9b61dab05846c1d
erolsson@kth.se <erolsson@kth.se>
2019-09-25 16:11:28 +0200

    "implementing umat in c++"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -35,7 +35,7 @@ double ms_stress(const Eigen::Matrix<double, 6, 1>& stress, double a1, double a2
 */
 extern "C" void uexternaldb_(const int* lop, const int* lrestart, const double* time, const double* dtime,
                              const int* kstep, const int* kinc) {
-    std::cout << "Current path is: "  << "std::filesystem::current_path()" << std::endl;
+    std::cout << "Current path is: "  <<  std::filesystem::current_path() << std::endl;
     if (*lop == 0) {
         std::cout << "Reading parameters" << std::endl;
         props = new SimulationParameters("material_parameters.par");
