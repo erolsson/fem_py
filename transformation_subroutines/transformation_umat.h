ab77eb937a2e66062dac2fe7ae8c9b7e5dd8084b
erolsson@kth.se <erolsson@kth.se>
2019-09-26 14:43:36 +0200

    "implementing umat in c++"

--- transformation_subroutines/transformation_umat.h
+++ transformation_subroutines/transformation_umat.h
@@ -8,10 +8,10 @@
 extern "C" void getoutdir_(char* outdir, int&, int);
 
 extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *sse, double *spd, double *scd,
-                      double *rpl, double *ddsddt, double *drplde, double *drpldt, double *stran, double *dstran, double *time,
-                      double *dtime, double *temp, double *dtemp, double *predef, double *dpred, char *cmname, int *ndi, int *nshr,
-                      int *ntens, int *nstatv, const double *props, int *nprops, double *coords, double *drot, double *pnewdt,
-                      double *celent, double *dfgrd0, double *dfgrd1, int *noel, int *npt, int *layer, int *kspt, int *kstep,
-                      int *kinc, short cmname_len);
+                      double *rpl, double *ddsddt, double *drplde, double *drpldt, double *stran, double *dstran,
+                      double *time, double *dtime, double *temp, double *dtemp, double *predef, double *dpred,
+                      char *cmname, int *ndi, int *nshr, int *ntens, int *nstatv, const double *props, int *nprops,
+                      double *coords, double *drot, double *pnewdt, double *celent, double *dfgrd0, double *dfgrd1,
+                      int *noel, int *npt, int *layer, int *kspt, int *kstep, int *kinc, short cmname_len);
 
 #endif //FEM_PY_TRANSFORMATION_UMAT_H
