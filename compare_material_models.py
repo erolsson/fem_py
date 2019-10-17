269017de1267dc27dd572d273455d6429272fad3
erolsson@kth.se <erolsson@kth.se>
2019-10-17 14:52:02 +0200

    "implementing umat in c++"

--- compare_material_models.py
+++ compare_material_models.py
@@ -29,14 +29,15 @@ pressure_z[:, 1] = 3000*np.sin(np.pi*time)
 
 args = {'pzz': pressure_z, 'increments': increments, 'material_parameters': test_material}
 
-for inc, lw in zip([1., 1e-1, 1e-2, 1e-3], [1, 2, 3, 4]):
+# for inc, lw in zip([1., 1e-1, 1e-2, 1e-3], [1, 2, 3, 4]):
+for inc, lw in zip([1.], [1]):
     for simulation in simulations:
         args['umat_file'] = simulation.umat_file
         args['simulation_name'] = simulation.name
         args['max_time_increment'] = inc
         e, s = simulation.model(**args)
         plt.figure(1)
-        plt.plot(e[:, 2], s[:, 2], '-' + simulation.color, lw=lw, label=simulation.label)
+        plt.plot(e[:, 2], s[:, 2], '-x' + simulation.color, lw=lw, label=simulation.label)
         plt.figure(2)
-        plt.plot(e[:, 2], e[:, 1], '-' + simulation.color, lw=lw, label=simulation.label)
+        plt.plot(e[:, 2], e[:, 1], '-x' + simulation.color, lw=lw, label=simulation.label)
 plt.show()
