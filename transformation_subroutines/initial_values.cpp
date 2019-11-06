2fd3f69a5a38cd1ba2dee371cf6b10012ea77ea5
erolsson@kth.se <erolsson@kth.se>
2019-11-06 12:54:20 +0100

    "implementing c++ user subroutines"

---
+++ transformation_subroutines/initial_values.cpp
@@ -0,0 +1,41 @@
+//
+// Created by erolsson on 06/11/2019.
+//
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <tuple>
+#include <vector>
+
+std::vector<std::tuple<unsigned, unsigned, double> > austenite;
+/*
+extern "C" void sdvini_(double* statev, const double* coords, const int* nstatev, const int* ncrds, const int* noel,
+        const int* npt, const int* layer, const int* kspt) {
+
+}
+*/
+
+extern "C" void uexternaldb_(const int* lop, const int* lrestart, const double* time, const double* dtime,
+        const int* kstep, const int* kinc) {
+    if (*lop == 0) {
+        // Beginning of analysis
+        std::vector<std::string> line_data;
+        std::string data_line;
+        std::ifstream austenite_file("austenite.dat");
+        while (getline(austenite_file, data_line)) {
+            std::stringstream line(data_line);
+            std::string val;
+            while (getline(line, val, ',')) {
+                line_data.push_back(val);
+            }
+            austenite.emplace_back(stoi(line_data[0]), stoi(line_data[0]), stod(line_data[0]));
+        }
+        for (auto line: austenite) {
+            auto [elem, gp, val] = line;
+            std::cout << elem << ", " << gp << ", " << val << std::endl;
+        }
+    }
+}
