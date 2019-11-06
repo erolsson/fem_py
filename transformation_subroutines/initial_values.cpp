99197e771f1210c254c0324e385d7c52304fe2ff
erolsson@kth.se <erolsson@kth.se>
2019-11-06 14:33:51 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/initial_values.cpp
+++ transformation_subroutines/initial_values.cpp
@@ -14,7 +14,7 @@
 #include <vector>
 
 extern "C" void getoutdir_(char* outdir, int&, int);
-extern "C" void getpartinfo_(const int* intnum, const int* jtype, char* cpname, int&, int* locnum, int* jrcd);
+extern "C" void getpartinfo_(const int* intnum, const int* jtype, char* cpname, int*, int* locnum, int* jrcd);
 
 struct PairHash {
 public:
@@ -38,7 +38,7 @@ extern "C" void sdvini_(double* statev, const double* coords, const int* nstatev
     {
         std::lock_guard<std::mutex> lock(part_info_mutex);
         std::cout << "lock assigned" << std::endl;
-        getpartinfo_(noel, &jtype, part_name_ptr, part_name_len, &user_elem_number, &error);
+        getpartinfo_(noel, &jtype, part_name_ptr, &part_name_len, &user_elem_number, &error);
         std::cout << "getpartinfo_ read" << std::endl;
     }
     std::string part_name(part_name_ptr);
