c9ebb6e0cc96499b914580347d95a65498d15db8
erolsson@kth.se <erolsson@kth.se>
2019-11-06 13:23:16 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/initial_values.cpp
+++ transformation_subroutines/initial_values.cpp
@@ -10,7 +10,8 @@
 #include <tuple>
 #include <vector>
 
-typedef std::string string;
+extern "C" void getoutdir_(char* outdir, int&, int);
+
 std::vector<std::tuple<unsigned, unsigned, double> > austenite;
 /*
 extern "C" void sdvini_(double* statev, const double* coords, const int* nstatev, const int* ncrds, const int* noel,
@@ -28,10 +29,15 @@ extern "C" void uexternaldb_(const int* lop, const int* lrestart, const double* 
         std::vector<std::string> line_data;
         std::string data_line;
         std::ifstream austenite_file("austenite.dat");
+        char out_dir_char[256];
+        int out_dir_len = 0;
+        getoutdir_(out_dir_char, out_dir_len, 256);
+        std::string out_dir(out_dir_char, out_dir_char+out_dir_len);
+        std::cout << out_dir << std::endl;
         while (getline(austenite_file, data_line)) {
             std::cout << "reading austine data" << std::endl;
             std::stringstream line(data_line);
-            string val;
+            std::string val;
             while (getline(line, val, ',')) {
                 line_data.push_back(val);
             }
