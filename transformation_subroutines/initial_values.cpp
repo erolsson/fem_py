550f6ff11e99b6b99be27a3ed23ab7dd2452d1b1
erolsson@kth.se <erolsson@kth.se>
2019-11-06 14:10:40 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/initial_values.cpp
+++ transformation_subroutines/initial_values.cpp
@@ -37,6 +37,7 @@ extern "C" void sdvini_(double* statev, const double* coords, const int* nstatev
         std::lock_guard<std::mutex> lock(part_info_mutex);
         std::cout << "lock assigned" << std::endl;
         getpartinfo_(noel, 1, part_name, user_elem_number_ptr, error);
+        std::cout << "getpartinfo_ read" << std::endl;
     }
     std::cout << part_name << ", " << user_elem_number_ptr << " " << error << std::endl;
 }
