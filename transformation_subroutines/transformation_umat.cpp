7066ddb36ecf7c9dc0a37cf6dadd3e9537284529
erolsson@kth.se <erolsson@kth.se>
2019-11-06 17:08:34 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -88,8 +88,8 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
         stilde -= state.total_back_stress();
     }
     bool plastic = params.plastic() && yield_function(stilde, sy) > 0;
-    bool phase_transformations = transformation_function(st, 0, temp, params) - state.fM() > 0;
-
+    // bool phase_transformations = transformation_function(st, 0, temp, params) - state.fM() > 0;
+    bool phase_transformations = false;
     bool elastic = !plastic && !phase_transformations;
     stress_vec = st;
     if (elastic) {     // Use the trial stress as the stress and the elastic stiffness matrix as the tangent
