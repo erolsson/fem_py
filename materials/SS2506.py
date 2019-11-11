2066f9ae4309f2aa4289380c2b9374b16b745eb2
erolsson@kth.se <erolsson@kth.se>
2019-11-11 10:53:29 +0100

    "implementing c++ user subroutines"

--- materials/SS2506.py
+++ materials/SS2506.py
@@ -39,7 +39,7 @@ class ElasticPlasticTransformMaterial:
         self.k = 0.04
         self.s_lim = 485.
         self.back_stresses = Cm.shape[0]
-        self.name = 'name'
+        self.name = name
 
         self.Mss = (-1./self.k*np.log(1-uniaxial_data[0]) - self.Ms - uniaxial_data[2]*(self.a1 + self.a2 + 2*self.a3/27) +
                     uniaxial_data[1])
--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -91,7 +91,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
 
     bool plastic = params.plastic() && yield_function(stilde, sy) > 0;
     bool phase_transformations = transformation_function(sigma_2, 0, temp, params) - state.fM() > 0;
-
+    std::cout << phase_transformations << std::endl;
     bool elastic = !plastic && !phase_transformations;
     stress_vec = sigma_2;
     if (elastic) {     // Use the trial stress as the stress and the elastic stiffness matrix as the tangent
