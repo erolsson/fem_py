79260dbd89c9fd86e5119bc7b743e3b0cdcee352
erolsson@kth.se <erolsson@kth.se>
2019-11-12 12:52:35 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -234,6 +234,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
             if (DL > 0) {
                 std::abort();
             }
+            std::cout << "Bijkl:" << std::endl << Bijkl.format(CleanFmt) << std::endl << std::endl;
             D_alg = Bijkl.inverse()*D_alg;
         }
     }
