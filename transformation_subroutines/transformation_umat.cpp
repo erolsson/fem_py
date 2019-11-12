12ad06ba0d665372fb36495de0030ceb661db519
erolsson@kth.se <erolsson@kth.se>
2019-11-12 09:59:35 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -234,6 +234,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
             }
             std::cout << "A: " << A << std::endl;
             D_alg = double_contract(static_cast<Matrix6x6>(Bijkl.inverse()), static_cast<Matrix6x6>(D_alg));
+            std::cout << "B_ijkl: " << std::endl << Bijkl.format(CleanFmt) << std::endl<< std::endl;
             std::cout << "D_alg: " << std::endl << D_alg.format(CleanFmt) << std::endl<< std::endl;
         }
     }
