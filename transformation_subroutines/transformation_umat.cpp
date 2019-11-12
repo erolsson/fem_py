e9e8127fea75ac88f62f6a86548c5eb6ae2b853a
erolsson@kth.se <erolsson@kth.se>
2019-11-12 10:55:45 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -226,6 +226,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
                     - 6*G*G*DL/s_eq_prime*(Aijkl - 1./A/B*double_contract(Aijkl, dsij_prime_dDL)*nij2.transpose());
         }
         if (DfM > 0) {
+            /*
             D_alg -= 4*G*G/B*DfM*params.R2()/params.sy0A()*nnt - 6*G*G*RA*DfM/s_eq_prime*Aijkl;
             Matrix6x6 Bijkl = I + K/3*params.dV()*delta_ij*bij.transpose()
                                 + 2*G*(RA + DfM*params.R2()/params.sy0A()*ds_eq_2_dfM)*nij2*bij.transpose();
@@ -233,6 +234,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
                 std::abort();
             }
             D_alg = double_contract(static_cast<Matrix6x6>(Bijkl.inverse()), static_cast<Matrix6x6>(D_alg));
+             */
         }
     }
 }
