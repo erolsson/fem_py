6b8136185fa90924b620f93781dd46391eb2c912
erolsson@kth.se <erolsson@kth.se>
2019-11-12 10:46:51 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -178,6 +178,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
                 ds_eq_2_dfM = -3*G/B*RA;
                 Vector6 dsijdDfM = -2*G*(RA + DfM*params.R2()/params.sy0A()*ds_eq_2_dfM)*nij2 - K/3*params.dV()*delta_ij;
                 dhdDfM = double_contract(bij, dsijdDfM) - 1;
+                std::cout << "DfM:" << DfM << std::endl;
             }
 
             nnt = nij2*nij2.transpose();
