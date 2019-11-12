0ba1b8d850cd12093c06b5601874932754191af3
erolsson@kth.se <erolsson@kth.se>
2019-11-12 10:59:16 +0100

    "implementing c++ user subroutines"

--- materials/SS2506.py
+++ materials/SS2506.py
@@ -31,7 +31,7 @@ class ElasticPlasticTransformMaterial:
         self.a2 = a[1]
         self.a3 = a[2]
 
-        self.R1 = 0.02
+        self.R1 = 0.02*0
         self.R2 = 0.02*0
 
         self.dV = 0.037
