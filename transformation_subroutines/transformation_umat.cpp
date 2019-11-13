df32b0e85d72759e3ac1e3725b746c73240d1a56
erolsson@kth.se <erolsson@kth.se>
2019-11-13 11:24:49 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -178,7 +178,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
                 double J2 = 0.5*double_contract(s, s);
                 bij = F*(params.a1()*delta_ij + 1.5*params.a2()*s/sqrt(3*J2)
                          + params.a3()*(contract(s, s) - 2./3*J2*delta_ij));
-                ds_eq_2_dfM = -3*G/B*RA;
+                ds_eq_2_dfM = -3*G*RA/B;
                 Vector6 dsijdDfM = -2*G*(RA + DfM*params.R2()/params.sy0A()*ds_eq_2_dfM)*nij2 - K/3*params.dV()*delta_ij;
                 dhdDfM = double_contract(bij, dsijdDfM) - 1;
             }
@@ -239,7 +239,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
             for (unsigned i = 3; i != 6; ++i) {
                 Bijkl(i, i) *= 2;
             }
-            /*
+
             std::cout << "J" << std::endl << (J).format(CleanFmt) << std::endl << std::endl;
             std::cout << "nnt" << std::endl << (nnt).format(CleanFmt) << std::endl << std::endl;
             std::cout << "Aijkl" << std::endl << (Aijkl).format(CleanFmt) << std::endl << std::endl;
@@ -250,7 +250,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
             std::cout << "B-1:" << std::endl << (Bijkl.inverse()).format(CleanFmt) << std::endl << std::endl;
             D_alg = Bijkl.inverse()*D_alg;
             std::cout << "D_alg2:" << std::endl << (D_alg).format(CleanFmt) << std::endl << std::endl;
-            */
+
         }
     }
 }
