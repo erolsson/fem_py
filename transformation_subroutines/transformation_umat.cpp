0d89d78702c2deb0695da584eba7232a21136d31
erolsson@kth.se <erolsson@kth.se>
2019-11-13 14:07:18 +0100

    "implementing c++ user subroutines"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -92,7 +92,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
     if (params.kinematic_hardening()) {
         stilde -= state.total_back_stress();
     }
-    std::cout << "s = " << stress_vec.format(CleanFmt) << std::endl;
+    std::cout << "s = " << sigma_t.format(CleanFmt) << std::endl;
     bool plastic = params.plastic() && yield_function(stilde, sy) > 0;
     bool phase_transformations = transformation_function(sigma_t, 0, temp, params) - state.fM() > 0;
     bool elastic = !plastic && !phase_transformations;
