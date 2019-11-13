250b98bf82b7b870a7b72efafa97d8380e8b2dd2
erolsson@kth.se <erolsson@kth.se>
2019-11-13 13:46:54 +0100

    "implementing c++ user subroutines"

--- materials/SS2506.py
+++ materials/SS2506.py
@@ -81,7 +81,7 @@ test_material = ElasticPlasticTransformMaterial(E=200e3, v=0.3, sy0M=1000000., s
 
 neu_sehitoglu = ElasticPlasticTransformMaterial(E=200e3, v=0.3, sy0M=1000., sy0A=420, Q=580., b=100.,
                                                 Cm=np.array([15432, 281622, 470894]),
-                                                gamma_m=np.array([5., 236., 2301.]), a=0*np.array([0.056/3, 0.028, 0.]),
+                                                gamma_m=np.array([5., 236., 2301.]), a=np.array([0.056/3, 0.028, 0.]),
                                                 Ms=169, name='NeuSehitoglu', uniaxial_data=[0.65, 22., 485], fM=0.65)
 
 if __name__ == '__main__':
