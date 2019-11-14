81bce517e6e4aee12ac5a3223abbe316cae85bc0
erolsson@kth.se <erolsson@kth.se>
2019-11-14 10:39:38 +0100

    "implementing c++ user subroutines"

--- graphs/transformation_function.py
+++ graphs/transformation_function.py
@@ -5,11 +5,11 @@ K = 200E3/3/(1-2*.03)
 
 
 def transformation_function(st, martensite_fraction):
-    sigma = st - K*0.037/3*(martensite_fraction - 0.65)
-    print sigma
+    sigma = np.outer(0*martensite_fraction + 1, st) - K*0.037/3*np.outer((martensite_fraction - 0.65), np.ones(3))
     a1 = 0.056/3
     a2 = 0
-    m_stress = a1*sigma + a2*sigma
+    m_stress = a1*np.sum(sigma, 1)
+    print "ms", m_stress
     k = 0.017
     ms = 169
     mss = -94.29909071576407
@@ -19,7 +19,7 @@ def transformation_function(st, martensite_fraction):
 
 if __name__ == '__main__':
     fm = np.linspace(0.6, 1.0, 1000)
-    s = 701.1
-    plt.plot(fm, transformation_function(s, fm))
+    s = np.array([0, 0, 701.1])
+    plt.plot(fm, transformation_function(s, fm), '*')
     print (transformation_function(s, 0.6501) - transformation_function(s, 0.65))/0.0001
     plt.show()
