899a1c23f056a300848f722f1cdbd0c4e2fa2108
erolsson@kth.se <erolsson@kth.se>
2019-11-17 09:21:48 +0100

    "implementing c++ user subroutines"

--- graphs/transformation_function.py
+++ graphs/transformation_function.py
@@ -22,8 +22,9 @@ def yield_function(st, martensite_fraction, plastic_strain):
     func = np.zeros((martensite_fraction.shape[0], plastic_strain.shape[0]))
     for i, phase in enumerate(martensite_fraction):
         for j, e in enumerate(plastic_strain):
-            seq2 = seqp - 3*G*(e + R1*phase)/(1+3*G*R2*phase/1000)
-            sy = 1000 + b*Q*e/(1 + b*e)
+            seq2 = seqp - 3*G*(e + R1*phase)/(1+3*G*R2*phase/neu_sehitoglu.sy0A)
+            sy0 = (f0 + phase)*neu_sehitoglu.sy0M + (1 - (f0 + phase))*neu_sehitoglu.sy0A
+            sy = neu_sehitoglu.sy0A + b*Q*e/(1 + b*e)
             func[i, j] = seq2 - sy
 
     return func
@@ -55,9 +56,7 @@ def transformation_function(st, martensite_fraction, plastic_strain):
             sigma = st - 2*G*(e + RA*phase)*nij - K*phase*0.037/3
             print sigma
             m_stress = a1*np.sum(sigma, 0) + a2*se
-            m_stress2 = a1*np.sum(sigma+1e-3, 0) + a2*se
-            print (1 - np.exp(-k*(ms + m_stress+1e-4 + mss - temp)) - (1 - np.exp(-k*(ms + m_stress + mss - temp))))/1e-4
-            print (1 - np.exp(-k*(ms + m_stress2 + mss - temp)) - (1 - np.exp(-k*(ms + m_stress + mss - temp))))/np.array([1e-3, 1e-3, 1e-3])
+            # m_stress2 = a1*np.sum(sigma+1e-3, 0) + a2*se
             func[i, j] = 1 - np.exp(-k*(ms + m_stress + mss - temp)) - (phase + f0)
     return func
 
@@ -66,7 +65,7 @@ if __name__ == '__main__':
     fm = np.linspace(0, 0.1, 1000)
     dl = np.linspace(0, 0.01, 1000)
     # s = np.array([-27.55,     -27.55,       1095])
-    s = np.array([2.5573293e-14,  1.1446052e-13,      701.14898])
+    s = np.array([ 3.382121e-13,  1.1446052e-13,       1541.767,])
     # hdl = transformation_function(s, 0, dl)[0, :]
     hfm = transformation_function(s, fm, 0)
     # print (hdl[1] - hdl[0])/(dl[1] - dl[0])
