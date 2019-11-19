0f326b383aa3adcbcf5b90400f765ce26af6092d
erolsson@kth.se <erolsson@kth.se>
2019-11-19 11:15:32 +0100

    "implementing c++ user subroutines"

--- materials/SS2506.py
+++ materials/SS2506.py
@@ -89,7 +89,7 @@ neu_sehitoglu = ElasticPlasticTransformMaterial(E=200e3, v=0.3, sy0M=1000., sy0A
                                                 Cm=np.array([15432, 281622, 470894]),
                                                 gamma_m=np.array([5., 236., 2301.]), a=np.array([0.056/3, 0.028, 0.]),
                                                 Ms=169, name='NeuSehitoglu', uniaxial_data=[0.65, 22., 485], fM=0.65,
-                                                beta=40000000000, alpha=4., n=4.)
+                                                beta=40000, alpha=4., n=4.)
 
 if __name__ == '__main__':
     print test_material.umat_parameters()
