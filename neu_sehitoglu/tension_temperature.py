abb07197727a25e12cf6993def5fefd1a83d10e9
erolsson@kth.se <erolsson@kth.se>
2019-11-20 15:24:54 +0100

    "implementing c++ user subroutines"

--- neu_sehitoglu/tension_temperature.py
+++ neu_sehitoglu/tension_temperature.py
@@ -9,13 +9,14 @@ from abaqus_material_test.material_test import one_element_abaqus
 from abaqus_material_test.one_element_input_file import BC
 
 umat_file = os.path.expanduser('~/fem_py/transformation_subroutines/transformation_subroutine.o')
+simulation_dir = os.path.expanduser('~/fem_py/abaqus_material_test/neu_sehitoglu/.o')
 
 for temp, color in zip([150., 22.], ['r', 'b']):
     data = np.genfromtxt(os.path.expanduser('~/phase_transformations/neu_sehitoglu/fig_4_' + str(int(temp)) + 'C'),
                          delimiter=',')
     plt.plot(data[:, 0], data[:, 1], color + '*')
     stress_bc = np.array([[0., 0], [1., data[-1, 1]]])
-    strain_bc = np.array([[0., 0], [1., data[0, 1]]])
+    strain_bc = np.array([[0., 0], [1., np.max(data[0, :])]])
 
     strain, stress, _, _ = one_element_abaqus('abaqus_material_test/neu_sehitoglu/', material=neu_sehitoglu,
                                               boundary_conditions=[BC(stress_bc, 'z', 'stress')],
