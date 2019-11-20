11d52535c4565cc1ca20f020afea66c312873fc1
erolsson@kth.se <erolsson@kth.se>
2019-11-20 16:22:11 +0100

    "implementing c++ user subroutines"

--- neu_sehitoglu/tension_temperature.py
+++ neu_sehitoglu/tension_temperature.py
@@ -15,8 +15,8 @@ for temp, color in zip([150., 22.], ['r', 'b']):
     data = np.genfromtxt(os.path.expanduser('~/phase_transformations/neu_sehitoglu/fig_4_' + str(int(temp)) + 'C'),
                          delimiter=',')
     plt.plot(data[:, 0], data[:, 1], color + '*')
-    stress_bc = np.array([[0., 0], [1., np.max(data[-1, :])]])
-    strain_bc = np.array([[0., 0], [1., np.max(data[0, :])]])
+    stress_bc = np.array([[0., 0], [1., np.max(data[:, 1])]])
+    strain_bc = np.array([[0., 0], [1., np.max(data[:, 0])]])
 
     strain, stress, _, _ = one_element_abaqus(simulation_dir, material=neu_sehitoglu,
                                               boundary_conditions=[BC(stress_bc, 'z', 'stress')],
