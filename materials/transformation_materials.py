901a5f2565187c4f4f2e2df032aba8a01de602ba
erolsson@kth.se <erolsson@kth.se>
2019-11-26 10:26:12 +0100

    "implementing c++ user subroutines"

--- materials/transformation_materials.py
+++ materials/transformation_materials.py
@@ -97,7 +97,7 @@ neu_sehitoglu = ElasticPlasticTransformMaterial(E=203.3e3, v=0.3, sy0M=813., sy0
 neu_sehitoglu = ElasticPlasticTransformMaterial(E=203.3e3, v=0.3, sy0M=813., sy0A=420., Q=0*2100., b=100.,
                                                 Cm=np.array([335485, 245783, 6853]),
                                                 gamma_m=np.array([1016.5, 185, 0.]),
-                                                a=np.array([0.056/3, 0.028, 0.]),
+                                                a=np.array([0.056, 0.028, 0.]),
                                                 Ms=169, name='NeuSehitoglu', uniaxial_data=[0.65, 22., 485], fM=0.65,
                                                 beta=1., alpha=4., n=4., sde=0.04)
 
