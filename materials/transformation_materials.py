717808b0879a97e347076d0e29dedf9108e92179
erolsson@kth.se <erolsson@kth.se>
2019-11-27 23:14:30 +0100

    "Calibrating model to hazar et. al"

--- materials/transformation_materials.py
+++ materials/transformation_materials.py
@@ -103,8 +103,8 @@ neu_sehitoglu = ElasticPlasticTransformMaterial(E=203.3e3, v=0.3, sy0M=813., sy0
 hazar_et_al = ElasticPlasticTransformMaterial(E=200.5e3, v=0.27, sy0M=801., sy0A=420., Q=180., b=100.,
                                               Cm=np.array([135e3, 700e3, 50e3]),
                                               gamma_m=np.array([950., 500., 50.]),
-                                              a=0.1175745*np.array([1., 0., 0.]),
-                                              Ms=220, name='SKF', Mss=-144.2, fM=0.78,
+                                              a=0.12*np.array([1., 0., 0.]),
+                                              Ms=220, name='SKF', Mss=-149.1, fM=0.78,
                                               beta=3.5, alpha=400., n=4., sde=0.04)
 hazar_et_al.k = 0.01
 hazar_et_al.dV = 0.0371
