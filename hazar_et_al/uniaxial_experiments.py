ddf97fd7c8cafcc3dbe17209fd90586aae5133fe
erolsson@kth.se <erolsson@kth.se>
2019-11-27 13:22:21 +0100

    "Calibrating model to hazar et. al"

--- hazar_et_al/uniaxial_experiments.py
+++ hazar_et_al/uniaxial_experiments.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-from collections import namedtuple
 import os
 
 import numpy as np
@@ -33,43 +32,44 @@ class Experiment:
 experiments = [Experiment(temperature=22, color='r'), Experiment(temperature=75, color='b'),
                Experiment(temperature=100, color='g'), Experiment(temperature=150, color='k')]
 
-for experiment in experiments:
-    plt.figure(0)
-    experiment.plot_stress_strain()
+if __name__ == '__main__':
+    for experiment in experiments:
+        plt.figure(0)
+        experiment.plot_stress_strain()
+        plt.figure(1)
+        experiment.plot_transformation()
+
+    par = None
+    k = 0
+    a = 0
+    Ms = 220
+    Mss = 0
+    for experiment in experiments[0:2]:
+        trans_stress = np.interp(experiment.transformation_data[:, 0], experiment.stress_strain[:, 0],
+                                 experiment.stress_strain[:, 1])
+        plt.figure(2)
+        k = 0.010
+        temp = experiment.temperature
+        y = -np.log(1 - experiment.transformation_data[:, 1])
+        if temp == 22.:
+            par = np.polyfit(trans_stress, y, 1)
+        plt.plot(trans_stress, y, 'x' + experiment.color, ms=12, mew=2)
+        if temp == 75.:
+            new_p = trans_stress[0]*par[0] + par[1]
+            k = (new_p - y)/(75-22)
+            a = par[0]/k
+            Mss = par[1]/k + 22 - Ms
+    print(k, a, Mss)
+
+    for experiment in experiments:
+        y = k*(Ms + a*experiment.stress_strain[:, 1] + Mss - experiment.temperature)
+        plt.figure(2)
+        plt.plot(experiment.stress_strain[:, 1], y, experiment.color)
+        plt.figure(1)
+        fM = 1 - np.exp(-y)
+        plt.plot(experiment.stress_strain[:, 0], fM, '--' + experiment.color)
+
     plt.figure(1)
-    experiment.plot_transformation()
-
-par = None
-k = 0
-a = 0
-Ms = 220
-Mss = 0
-for experiment in experiments[0:2]:
-    trans_stress = np.interp(experiment.transformation_data[:, 0], experiment.stress_strain[:, 0],
-                             experiment.stress_strain[:, 1])
-    plt.figure(2)
-    k = 0.010
-    temp = experiment.temperature
-    y = -np.log(1 - experiment.transformation_data[:, 1])
-    if temp == 22.:
-        par = np.polyfit(trans_stress, y, 1)
-    plt.plot(trans_stress, y, 'x' + experiment.color, ms=12, mew=2)
-    if temp == 75.:
-        new_p = trans_stress[0]*par[0] + par[1]
-        k = (new_p - y)/(75-22)
-        a = par[0]/k
-        Mss = par[1]/k + 22 - Ms
-print(k, a, Mss)
-
-for experiment in experiments:
-    y = k*(Ms + a*experiment.stress_strain[:, 1] + Mss - experiment.temperature)
-    plt.figure(2)
-    plt.plot(experiment.stress_strain[:, 1], y, experiment.color)
-    plt.figure(1)
-    fM = 1 - np.exp(-y)
-    plt.plot(experiment.stress_strain[:, 0], fM, '--' + experiment.color)
-
-plt.figure(1)
-plt.xlim(0, 0.015)
-plt.ylim(0.78, 1.)
-plt.show()
+    plt.xlim(0, 0.015)
+    plt.ylim(0.78, 1.)
+    plt.show()
---
+++ hazar_et_al/uniaxial_simulations.py
@@ -0,0 +1,30 @@
+import os
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from uniaxial_experiments import experiments
+from materials.transformation_materials import hazar_et_al
+from abaqus_material_test.material_test import one_element_abaqus
+from abaqus_material_test.one_element_input_file import BC
+
+umat_file = os.path.expanduser('~/fem_py/transformation_subroutines/transformation_subroutine.o')
+simulation_dir = os.path.expanduser('~/fem_py/abaqus_material_test/hazar_et_al')
+
+for experiment in experiments:
+    experiment.plot_stress_strain()
+    stress_bc = BC([[0, 0], [1., 1.2*experiment.stress_strain[-1, 1]]])
+    if experiment.stress_strain[-1, 1] > 0:
+        name = 'tension'
+    else:
+        name = 'compression'
+    e, s, _, _ = one_element_abaqus(simulation_dir, material=hazar_et_al,
+                                    boundary_conditions=[BC(stress_bc, 'z', 'stress')],
+                                    simulation_name=name + '_' + str(int(experiment.temperature)),
+                                    temperature=np.array([[0, experiment.temperature], [1, experiment.temperature]]),
+                                    user_subroutine=umat_file,
+                                    max_increment=0.1)
+    plt.plot(e, s, '--' + experiment.color)
+
+plt.show()
