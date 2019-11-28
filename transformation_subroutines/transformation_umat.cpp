de3ce6366411578c7474d03332ad34f090739e33
erolsson@kth.se <erolsson@kth.se>
2019-11-28 19:12:23 +0100

    "Changed plastic strain transformation"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -148,14 +148,15 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
         while (residual > 1e-15) {
             ++iter;
             sigma_2 = sigma_t;
+            double dfsbdL = params.alpha()/(1+params.alpha()*DL)*(1 - fsb2);
+            c = params.beta()*tr_func*params.n()*pow(fsb2, params.n() - 1)*dfsbdL;
             DfM = DfM_stress + c*DL;
             double dDL = 0;
             double dDfM = 0;
             B = 1 + 3*G*params.R2()*DfM/params.sy0A();
             s_eq_2 = (s_eq_prime - 3*G*(DL + params.R1()*DfM))/B;
             double I1 = sigma_t[0] + sigma_t[1] + sigma_t[2] - 3*K*params.dV()*DfM;
-            double dfsbdL = params.alpha()/(1+params.alpha()*DL)*(1 - fsb2);
-            c = params.beta()*tr_func*params.n()*pow(fsb2, params.n() - 1)*dfsbdL;
+
             h = 1 - tr_func - (state.fM() + DfM);
 
             dfdDfM = -3*G*RA/B - params.a()*K*params.dV() - (params.sy0M() - params.sy0A());
