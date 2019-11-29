3e3429a3601b814ea013fd764fdee7224e68b44b
erolsson@kth.se <erolsson@kth.se>
2019-11-29 09:44:57 +0100

    "Changed plastic strain transformation"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -88,6 +88,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
     double sy = params.sy0M()*state.fM() + params.sy0A()*(1-state.fM()) + state.R();
 
     Vector6 sigma_t = stress_vec + Del*de;  // Trial stress
+    std::cout << "Trial stress: " << sigma_t.transpose.format(CleanFmt) << std::endl;
     Vector6 sij_t = deviator(sigma_t);
 
     Vector6 stilde = sij_t;
