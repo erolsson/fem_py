78d3ace838d38a4315975c1df28f2267ea6091d5
erolsson@kth.se <erolsson@kth.se>
2019-12-05 10:19:28 +0100

    "Changed plastic strain transformation"

--- materials/transformation_materials.py
+++ materials/transformation_materials.py
@@ -6,7 +6,8 @@ import numpy as np
 
 # noinspection PyPep8Naming
 class ElasticPlasticTransformMaterial:
-    def __init__(self, E, v, sy0M, sy0A, Q, b, Cm, gamma_m, a, Ms, name, Mss, fM, beta, alpha, n, sde):
+    def __init__(self, E, v, sy0M, sy0A, Q, b, Cm, gamma_m, a, Ms, name, Mss, fM, beta, alpha, n, sde,
+                 g0, g1, g2, g_mean, g_std):
         # Elastic parameters
         self.E = float(E)
         self.v = float(v)
@@ -48,6 +49,13 @@ class ElasticPlasticTransformMaterial:
 
         self.sde = sde
 
+        self.g0 = g0
+        self.g1 = g1
+        self.g2 = g2
+
+        self.g_mean = g_mean
+        self.g_std = g_std
+
         self.back_stresses = Cm.shape[0]
         self.name = name
         if isinstance(Mss, numbers.Real):
@@ -75,7 +83,6 @@ class ElasticPlasticTransformMaterial:
     def umat_depvar(self):
         return 6 + self.back_stresses*6
 
-
     def umat_parameters(self):
         parameters = [self.E, self.v, self.sy0M, self.sy0A,  self.Q, self.b, self.gamma_m.shape[0]]
         kinematic_hardening_params = []
@@ -83,7 +90,7 @@ class ElasticPlasticTransformMaterial:
             kinematic_hardening_params += [C, g]
         return parameters + kinematic_hardening_params + [self.R1, self.R2, self.dV, self.Ms, self.Mss, self.k,
                                                           self.a1, self.a2, self.a3, self.beta, self.alpha, self.n,
-                                                          self.sde]
+                                                          self.sde, self.g0, self.g1, self.g2, self.g_mean, self.g_std]
 
     def sy0(self, fm):
         return fm*self.sy0M + (1 - fm)*self.sy0A
@@ -93,21 +100,24 @@ test_material = ElasticPlasticTransformMaterial(E=200e3, v=0.3, sy0M=1000000., s
                                                 Cm=np.array([135e3, 700e3, 50e3]),
                                                 gamma_m=np.array([950., 500., 50.]), a=[0.056, 0.028, 0.],
                                                 Ms=169, name='testMaterial', Mss=[0.8, 22., 485], fM=0.8,
-                                                beta=0, alpha=4., n=4., sde=0.04)
+                                                beta=0, alpha=4., n=4., sde=0.04, g0=60, g1=176000, g2=5.2,
+                                                g_mean=57, g_std=350)
 
 neu_sehitoglu = ElasticPlasticTransformMaterial(E=203.3e3, v=0.3, sy0M=813., sy0A=420., Q=0*2100., b=100.,
                                                 Cm=np.array([335485, 245783, 6853]),
                                                 gamma_m=np.array([1016.5, 185, 0.]),
                                                 a=np.array([0.056/3, 0.028, 0.]),
                                                 Ms=169, name='NeuSehitoglu', Mss=[0.65, 22., 485], fM=0.65,
-                                                beta=1., alpha=4., n=4., sde=0.04)
+                                                beta=1., alpha=4., n=4., sde=0.04, g0=60, g1=176000, g2=5.2,
+                                                g_mean=57, g_std=350)
 
-hazar_et_al = ElasticPlasticTransformMaterial(E=200.5e3, v=0.27, sy0M=1061., sy0A=420., Q=180., b=100.,
+hazar_et_al = ElasticPlasticTransformMaterial(E=200.5e3, v=0.27, sy0M=10000000., sy0A=420., Q=180., b=100.,
                                               Cm=np.array([135e3, 700e3, 50e3]),
                                               gamma_m=np.array([950., 500., 50.]),
                                               a=0.0677272727272727*np.array([1., 0., 0.]),
                                               Ms=220, name='SKF', Mss=-104.13818181818179, fM=0.78,
-                                              beta=30.5, alpha=146., n=4., sde=0.04)
+                                              beta=30.5, alpha=146., n=4., sde=0.04, g0=60, g1=176000, g2=5.2,
+                                              g_mean=57, g_std=350)
 hazar_et_al.k = 0.01
 hazar_et_al.dV = 0.0371
 hazar_et_al.R1 = -0.07032324
