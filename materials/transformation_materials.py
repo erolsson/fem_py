6749720ee0c445f8a6fc82c49cab68dd0614e6fe
erolsson@kth.se <erolsson@kth.se>
2019-12-05 17:19:33 +0100

    "Changed plastic strain transformation"

--- materials/transformation_materials.py
+++ materials/transformation_materials.py
@@ -114,7 +114,7 @@ neu_sehitoglu = ElasticPlasticTransformMaterial(E=203.3e3, v=0.3, sy0M=813., sy0
 hazar_et_al = ElasticPlasticTransformMaterial(E=200.5e3, v=0.27, sy0M=1016, sy0A=420., Q=180., b=100.,
                                               Cm=np.array([135e3, 700e3, 50e3]),
                                               gamma_m=np.array([950., 500., 50.]),
-                                              a=0*0.0677272727272727*np.array([1., 0., 0.]),
+                                              a=0.0677272727272727*np.array([1., 0., 0.]),
                                               Ms=220, name='SKF', Mss=-104.13818181818179, fM=0.78,
                                               beta=0*30.5, alpha=146., n=4., sde=0.04, g0=60, g1=0*176000, g2=5.2,
                                               g_mean=57, g_std=350)
