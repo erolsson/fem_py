7685093718d48a96d1521bc09075b0b066349899
erolsson@kth.se <erolsson@kth.se>
2019-12-05 15:03:12 +0100

    "Changed plastic strain transformation"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -100,6 +100,7 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
     }
     bool plastic = params.plastic() && yield_function(sigma_t, state.total_back_stress(), sy, params) > 0;
     bool stress_transformations = stress_transformation_function(sigma_t, temp, params, state.fM()) >= 0;
+    std::cout << "Stress tr: " << stress_transformation_function(sigma_t, temp, params, state.fM()) << std::endl;
     bool strain_transformations = params.beta() > 0 && plastic;
     bool elastic = !plastic && !stress_transformations;
     if (elastic) {     // Use the trial stress as the stress and the elastic stiffness matrix as the tangent
