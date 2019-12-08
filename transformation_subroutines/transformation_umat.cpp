c4865a8340298c3b462299401eedd72ef78403ff
erolsson@kth.se <erolsson@kth.se>
2019-12-08 22:34:17 +0100

    "Changed plastic strain transformation"

--- transformation_subroutines/transformation_umat.cpp
+++ transformation_subroutines/transformation_umat.cpp
@@ -388,8 +388,9 @@ extern "C" void umat_(double *stress, double *statev, double *ddsdde, double *ss
             Bijkl += 2*G*(RA + DfM*params.R2()/params.sy0A()*ds_eq_2_dfM)*nij2*Fskl.transpose()
                     + K*params.dV()*delta_ij*Fskl.transpose();
             if (npt == 1.) {
+                std::cout << "Fskl: " << Fskl.transpose().format(CleanFmt) << std::endl;
                 std::cout << "Time: " << *time << std::endl;
-                std::cout << "ds_eq_2_dfM: " << ds_eq_2_dfM << std::endl;
+                std::cout << "ds_eq_2_dfM: " << ds_eq_2_dfM << "  "  <<  std::endl;
                 std::cout << Bijkl.format(CleanFmt) << std::endl;
             }
             for (unsigned i = 3; i != 6; ++i) {
