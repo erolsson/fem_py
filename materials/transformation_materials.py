c03efe7b4abea8a6d27b77b64bb3bad3aba47708
erolsson@kth.se <erolsson@kth.se>
2019-12-12 19:36:48 +0100

    "Changed plastic strain transformation"

--- materials/transformation_materials.py
+++ materials/transformation_materials.py
@@ -123,8 +123,8 @@ hazar_et_al = ElasticPlasticTransformMaterial(E=200.5e3, v=0.27, sy0M=1016, sy0A
                                               fsb0=0.1)
 hazar_et_al.k = 0.01
 hazar_et_al.dV = 0.037
-hazar_et_al.R1 = 0.02
-hazar_et_al.R2 = 0.01
+hazar_et_al.R1 = 0.01
+hazar_et_al.R2 = 0.001
 parameters = {'beta': 500, 'alpha': 100, 'a1': 0.05, 'Mss': -86., 'fsb0': 0.1,
               'R1': 0.02, 'R2': 0.01, 'dV': 0.037, 'g1': 100,
               'g_std': 200, 'g0': 10}
