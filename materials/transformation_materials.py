b883694b0ae3af15de0a714e2702fa453312ea7e
erolsson@kth.se <erolsson@kth.se>
2019-12-12 14:37:04 +0100

    "Changed plastic strain transformation"

--- materials/transformation_materials.py
+++ materials/transformation_materials.py
@@ -120,7 +120,7 @@ hazar_et_al = ElasticPlasticTransformMaterial(E=200.5e3, v=0.27, sy0M=1016, sy0A
                                               Ms=220, name='SKF', Mss=-86.58722673702243, fM=0.78,
                                               beta=4.92067433e+02, alpha=1.06625032e+02, n=4., sde=0.04, g0=1e6,
                                               g1=1.78036231e+02, g2=0, g_mean=0, g_std=9.43482534e+01,
-                                              fsb0=2.83373527e-03)
+                                              fsb0=6.40671990e-02)
 hazar_et_al.k = 0.01
 hazar_et_al.dV = 2.35190122e-02
 hazar_et_al.R1 = 2.46866223e-02
