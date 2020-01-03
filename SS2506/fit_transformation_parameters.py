08133de21392daf0e482e319520e3336b2362a06
erolsson@kth.se <erolsson@kth.se>
2020-01-03 22:17:12 +0100

    "Changed plastic strain transformation"

--- SS2506/fit_transformation_parameters.py
+++ SS2506/fit_transformation_parameters.py
@@ -85,6 +85,6 @@ if __name__ == '__main__':
     experimental_data[:, 0:2] = stress_strain
     experimental_data[:, 2] = strains[:, 1]
     parameters = {'a1': 0.02,
-                  'R1': 0.02, 'R2': 0.02, 'dV': 0.037, 'Mss': -30}
+                  'R1': 0.02, 'R2': 0.02, 'dV': 0.037, 'Mss': -10}
     print(fmin(residual, list(parameters.values()), args=(list(parameters.keys()), experimental_data),
                maxfun=1e6, maxiter=1e6))
