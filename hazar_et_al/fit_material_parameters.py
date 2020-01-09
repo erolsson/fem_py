807a5d3921d60ee3d7f2daf0e5d81c0d74ba3f65
erolsson@kth.se <erolsson@kth.se>
2020-01-09 16:10:09 +0100

    "Changed plastic strain transformation"

--- hazar_et_al/fit_material_parameters.py
+++ hazar_et_al/fit_material_parameters.py
@@ -100,6 +100,7 @@ def residual(par, *data):
         else:
             print(e_exp[::-1], e_fem[::-1, 2], s_fem[::-1, 2])
             s_intep = np.interp(e_exp[::-1], e_fem[::-1, 2], s_fem[::-1, 2])
+            print(s_intep)
         stress_residual = np.sum((1 - s_intep[abs(s_exp) > 500]/s_exp[abs(s_exp) > 500])**2) / \
             s_exp[abs(s_exp) > 500].shape[0]
         plt.figure(fig)
