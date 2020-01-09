305ff054e1d24eca2d399ac66440d588703f8c2a
erolsson@kth.se <erolsson@kth.se>
2020-01-09 11:11:32 +0100

    "Changed plastic strain transformation"

--- hazar_et_al/fit_material_parameters.py
+++ hazar_et_al/fit_material_parameters.py
@@ -151,7 +151,7 @@ if __name__ == '__main__':
     # parameters = {'beta': 916, 'g0': 0., 'g1': 5.244, 'M_sigma': 22, 'M_d': 350}
     bounds = {'M_sigma': (22., 75), 'M_d': (160, None)}
     # parameters = {'beta': 700, 'a1': 0.034709588, 'Mss': -150}
-    experiments = experiments[1:2]
+    experiments = experiments[0:1]
     plt.figure(0)
     plt.ion()
     plt.show()
