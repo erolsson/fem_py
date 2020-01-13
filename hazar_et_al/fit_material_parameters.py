8db1f8dae7d3fed321d950829de73c3c699521e6
erolsson@kth.se <erolsson@kth.se>
2020-01-13 12:50:50 +0100

    "Changed plastic strain transformation"

--- hazar_et_al/fit_material_parameters.py
+++ hazar_et_al/fit_material_parameters.py
@@ -152,12 +152,12 @@ def residual(par, *data):
 
 
 if __name__ == '__main__':
-    parameters = {'beta': 700, 'fsb0': 0.147, 'alpha': 110.2, 'R1': 0.0181,
-                  'R2': 0.00547, 'a1': 0.01955, 'Mss': -130.979}
-    # parameters = {'beta': 916, 'g0': 0., 'g1': 5.244, 'M_sigma': 22, 'M_d': 350}
+    # parameters = {'beta': 700, 'fsb0': 0.147, 'alpha': 110.2, 'R1': 0.0181,
+    #              'R2': 0.00547, 'a1': 0.01955, 'Mss': -130.979}
+    parameters = {'beta': 916, 'g0': 0., 'g1': 5.244, 'M_sigma': 22, 'M_d': 350}
     bounds = {'M_sigma': (22., 75), 'M_d': (160, None)}
     # parameters = {'beta': 800, 'alpha': 150, 'fsb0': 0.2}
-    experiments = experiments[0:1]
+    experiments = experiments[0:4]
     plt.figure(0)
     plt.ion()
     plt.show()
