8d3277215db2293bedd348267419d39b5045e985
erolsson@kth.se <erolsson@kth.se>
2020-01-28 11:30:37 +0100

    "Changed plastic strain transformation"

--- hazar_et_al/uniaxial_simulations.py
+++ hazar_et_al/uniaxial_simulations.py
@@ -52,6 +52,6 @@ for i, fig in enumerate('abcd'):
     else:
         plt.ylabel('Fraction martensite [-]')
     if i == 0:
-        plt.legend(loc='upper left', bbox_to_anchor=(0., 0.89), framealpha=0.9)
+        plt.legend(loc='upper left', bbox_to_anchor=(0.5, 0.5), framealpha=0.9)
     plt.tight_layout()
 plt.show()
